95a223d7c21e4121486570419f10927d2e7a233e
AdamWendell <Adam@websmith.se>
2016-02-29 12:18:53 +0100

    Added admin.js and minprofil.ejs

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.use('/minprofil', require('./minprofil'));
+router.use();
 
 module.exports = router;
