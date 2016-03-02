f1f2c98e1332ac117a768a8170fd122d81704986
AdamWendell <Adam@websmith.se>
2016-03-02 14:32:02 +0100

    Working on the felanmalningar portion

--- routes/admin/felanmalningar.js
+++ routes/admin/felanmalningar.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var FelanmModel = require('../../mongotest');
+
+
 
 /* GET home page. */
 router.get('/felanmalningar', function(req, res, next) {
-  res.render('./admin/felanmalningar');
+  FelanmModel.find({klar: false}).then((felanmalningar) => {
+
+    res.render('./admin/felanmalningar', {felanmalningar: felanmalningar});
+  }, (err) => {});
+
 });
+
+router.post('/felanmalningar/:id', function (req, res, next) {
+  FelanmModel.findByIdAndUpdate(req.params.id, {klar: true}, (err, updated) => {
+    if (err) {
+      next(err);
+    } else {
+      res.redirect('/admin/felanmalningar')
+    }
+  })
+})
 
 module.exports = router;
