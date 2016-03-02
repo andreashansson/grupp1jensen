c20dfac6bdae91214323d49f203418433623c762
AdamWendell <Adam@websmith.se>
2016-03-02 20:34:17 +0100

    Added create functionallity on felanmalan.

--- routes/anvandare/felanmalan.js
+++ routes/anvandare/felanmalan.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var FelanmModel = require('../../mongotest')
 /* GET home page. */
 router.get('/felanmalan', function(req, res, next) {
   res.render('./anvandare/felanmalan');
 });
 
+router.post('/felanmalan', function (req, res, next) {
+  req.body.klar = false;
+
+  FelanmModel.create(req.body, (err, savedFelanm) => {
+    if (err) {
+      next(err);
+    } else {
+      console.log(savedFelanm);
+    }
+  })
+
+})
 module.exports = router;
